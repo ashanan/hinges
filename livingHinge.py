7137a896be614b9ecbd072c6ea8bf315bf009277
Avner Shanan <ashanan@gmail.com>
2012-03-08 20:31:08 -0600

    Creates unstaggered lines correctly.  Saves test output as test.svg.

--- livingHinge.py
+++ livingHinge.py
@@ -11,6 +11,29 @@ cut_length = 13.3
 hinge_height = 90
 hinge_width = 110
 
-cut_columns = math.floor(hinge_width / (kerf + cut_distance))
+layer = svgcuts.Layer(hinge_width, hinge_height)
+
+cut_columns = int(math.floor(hinge_width / (kerf + cut_distance)))
+cut_rows = int(math.floor(hinge_height / (cut_length + gap)))
 
 print cut_columns
+print cut_rows
+
+#[ svgcuts.Point(n*(kerf + cut_distance), (n+1)*kerf + n*cut_distance) for n in range(0,cut_columns)]
+# [ svgcuts.Line(svgcuts.Point(n*(kerf + cut_distance),0), svgcuts.Point((n+1)*kerf + n*cut_distance),0) for n in range(0,cut_columns)]
+# [layer.add_line(svgcuts.Line(svgcuts.Point(n*(kerf + cut_distance), 0),svgcuts.Point((n+1)*kerf + n*cut_distance,0))) for n in range(0,cut_columns)]
+
+for x in range(0, cut_columns):
+    for y in range(0, cut_rows):
+        upperLeftPoint = svgcuts.Point(x * (kerf + cut_distance), y * (cut_length + gap))
+        upperRightPoint = svgcuts.Point((x+1)*kerf + x*cut_distance, y * (cut_length + gap))
+
+        lowerLeftPoint = svgcuts.Point(x * (kerf + cut_distance), (y + 1) * cut_length + y * gap)
+        lowerRightPoint = svgcuts.Point((x + 1)*kerf + x*cut_distance, (y + 1) * cut_length + y * gap)
+
+        layer.add_line(svgcuts.Line(upperLeftPoint, upperRightPoint))
+        layer.add_line(svgcuts.Line(upperRightPoint, lowerRightPoint))
+        layer.add_line(svgcuts.Line(lowerRightPoint, lowerLeftPoint))
+        layer.add_line(svgcuts.Line(lowerLeftPoint, upperLeftPoint))
+
+layer.write("test.svg")

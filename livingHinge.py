e47a62f99c300fe72fadde87cc8bec96e4f0463b
Avner Shanan <ashanan@gmail.com>
2012-03-08 23:14:28 -0600

    Hinge height and width determinable by cli arguments

--- livingHinge.py
+++ livingHinge.py
@@ -1,16 +1,21 @@
+from sys import argv
 import svgcuts
 import math
 
 #consants based on http://www.thingiverse.com/thing:15845
 #units = mm
-
 kerf = .08
 gap = 1.85
 cut_distance = 1.5
 cut_length = 13.3
 offset_cut_length = cut_length / 2
-hinge_height = 90
-hinge_width = 110
+
+if len(argv) > 1:
+    hinge_height = int(argv[1])
+    hinge_width = int(argv[2])
+else:
+    hinge_height = 90
+    hinge_width = 110
 
 layer = svgcuts.Layer(hinge_width, hinge_height)
 

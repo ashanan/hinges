bbec23169b320a0a31dbe4b4e1c3bd1822fcac4f
Avner Shanan <ashanan@gmail.com>
2012-03-10 10:30:33 -0600

    Updated livingHinge.py to use Hinge object

--- livingHinge.py
+++ livingHinge.py
@@ -1,6 +1,5 @@
 from sys import argv
-import svgcuts
-import math
+import hinge
 
 #consants based on http://www.thingiverse.com/thing:15845
 #units = mm
@@ -17,46 +16,6 @@ else:
     hinge_height = 90
     hinge_width = 110
 
-layer = svgcuts.Layer(hinge_width, hinge_height)
+h = hinge.Hinge(hinge_width, hinge_height, cut_distance, cut_length, kerf, gap)  
 
-cut_columns = int(math.floor(hinge_width / (kerf + cut_distance)))
-cut_rows = int(math.floor(hinge_height / (cut_length + gap)))
-
-for x in range(0, cut_columns):
-    for y in range(0, cut_rows):
-        if cut_length == offset_cut_length:
-            cut_length *= 2
-
-        if x % 2 == 0:
-            offset = 0
-        elif y != 0:
-            offset = -offset_cut_length
-
-        if (y == 0 or y == cut_rows) and x % 2 == 1:
-            cut_length = offset_cut_length
-
-        upperLeftPoint = svgcuts.Point(x * (kerf + cut_distance), y * (cut_length + gap) + offset)
-        upperRightPoint = svgcuts.Point((x+1)*kerf + x*cut_distance, y * (cut_length + gap) + offset)
-
-        lowerLeftPoint = svgcuts.Point(x * (kerf + cut_distance), (y + 1) * cut_length + y * gap + offset)
-        lowerRightPoint = svgcuts.Point((x + 1)*kerf + x*cut_distance, (y + 1) * cut_length + y * gap + offset)
-
-        layer.add_line(svgcuts.Line(upperLeftPoint, upperRightPoint))
-        layer.add_line(svgcuts.Line(upperRightPoint, lowerRightPoint))
-        layer.add_line(svgcuts.Line(lowerRightPoint, lowerLeftPoint))
-        layer.add_line(svgcuts.Line(lowerLeftPoint, upperLeftPoint))
-    else:
-        if x % 2 == 1:
-            y += 1
-            upperLeftPoint = svgcuts.Point(x * (kerf + cut_distance), y * (cut_length + gap) + offset)
-            upperRightPoint = svgcuts.Point((x+1)*kerf + x*cut_distance, y * (cut_length + gap) + offset)
-
-            lowerLeftPoint = svgcuts.Point(x * (kerf + cut_distance), (y + 1) * cut_length + (y-1) * gap + offset - offset_cut_length)
-            lowerRightPoint = svgcuts.Point((x + 1)*kerf + x*cut_distance, (y + 1) * cut_length + (y-1) * gap + offset - offset_cut_length)
-
-            layer.add_line(svgcuts.Line(upperLeftPoint, upperRightPoint))
-            layer.add_line(svgcuts.Line(upperRightPoint, lowerRightPoint))
-            layer.add_line(svgcuts.Line(lowerRightPoint, lowerLeftPoint))
-            layer.add_line(svgcuts.Line(lowerLeftPoint, upperLeftPoint))
-
-layer.write("test.svg")
+h.write("test.svg")
